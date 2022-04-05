2155bb822598817ae55af3b66a8b33868a51e6e9
chaudhary-pra834 <praveenchaudhary834@gmail.com>
2022-04-06 00:34:06 +0530

    .

---
+++ Day 1/sortingarray.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class sortingarray {
+  public static void main(String args[]) {
+      int [] rollName={24,45,56,89,48,20};
+      Arrays.sort(rollName);
+      for(int temp:rollName)
+      System.out.print(temp+" ");
+  }
+
+  }
